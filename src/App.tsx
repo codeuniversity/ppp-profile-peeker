43a6579cd0d377e164cdc118b26ad92eecd4f1da
alexmorten <alexandrius.martinus@gmail.com>
2018-10-22 10:49:38 +0200

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,53 @@
-import * as React from 'react';
-import './App.css';
-
-
+import * as React from "react";
+import "./App.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 interface Message {
-  average: number
-  current: number
+  average: number;
+  current: number;
 }
 
 interface State {
-  average: number
-  current: number
+  average: number;
+  current: number;
 }
 
 class App extends React.Component<{}, State> {
-  constructor(props: {}){
+  constructor(props: {}) {
     super(props);
     this.state = {
       average: 0,
       current: 0,
-    }
+    };
   }
 
-  public componentDidMount(){
-    const socket = new WebSocket('ws://localhost:4000');
+  public componentDidMount() {
+    const socket = new WebSocket("ws://localhost:4000");
 
-    socket.addEventListener('open', (event) => {
-        socket.send('Hello Server!');
+    socket.addEventListener("open", event => {
+      socket.send("Hello Server!");
     });
-    socket.addEventListener('message', (event) => {
-      const data = JSON.parse(event.data) as Message
-      this.setState(data)
+    socket.addEventListener("message", event => {
+      const data = JSON.parse(event.data) as Message;
+      this.setState(data);
     });
-
   }
 
   public render() {
     const { average, current } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <CssBaseline>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
 
-        <div>
-          <p>Lifetime Average CPU Temp is {average.toFixed(2)}C</p>
-          <p>Current CPU Temp is {current.toFixed(2)}</p>
+          <div>
+            <p>Lifetime Average CPU Temp is {average.toFixed(2)}C</p>
+            <p>Current CPU Temp is {current.toFixed(2)}</p>
+          </div>
         </div>
-
-      </div>
+      </CssBaseline>
     );
   }
 }

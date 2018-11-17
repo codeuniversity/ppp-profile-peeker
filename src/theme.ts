079321354640aed3efbd1cf25d308b48f3ebea53
alexmorten <alexandrius.martinus@gmail.com>
2018-11-17 19:53:24 +0100

    remove theme overrides

--- src/theme.ts
+++ src/theme.ts
@@ -1,26 +1,6 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-import { PaletteColor } from "@material-ui/core/styles/createPalette";
-
-const primary: PaletteColor = {
-  light: "#6d6d6d",
-  main: "#424242",
-  dark: "#1b1b1b",
-  contrastText: "#ffffff",
-};
-
-const secondary: PaletteColor = {
-  light: "#5f5fc4",
-  main: "#283593",
-  dark: "#001064",
-  contrastText: "#eeeeee",
-};
-
 const theme = createMuiTheme({
-  palette: {
-    primary,
-    secondary,
-  },
   typography: {
     useNextVariants: true,
   },

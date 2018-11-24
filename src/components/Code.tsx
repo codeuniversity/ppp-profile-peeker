624310c7257fa51e71166a02621f928c146b48f9
alexmorten <alexandrius.martinus@gmail.com>
2018-11-24 19:53:24 +0100

    add class name prop to code component

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,27 +1,29 @@
 import * as React from "react";
 import { Theme, WithStyles, withStyles, createStyles } from "@material-ui/core";
 import grey from "@material-ui/core/colors/grey";
+import classnames from "classnames";
 
 const styles = (theme: Theme) =>
   createStyles({
     container: {
-      backgroundColor: grey[300],
+      backgroundColor: grey[200],
       padding: theme.spacing.unit,
       borderRadius: 3,
     },
     code: {
-      whiteSpace: "pre-wrap",
+      whiteSpace: "pre",
     },
   });
 
 interface Props extends WithStyles<typeof styles> {
   margin?: string | number;
   display?: "block" | "inline-block";
+  className?: string;
 }
 
-const Code: React.SFC<Props> = ({ classes, children, margin, display = "inline-block" }) => {
+const Code: React.SFC<Props> = ({ classes, children, margin, className, display = "inline-block" }) => {
   return (
-    <div className={classes.container} style={{ margin, display }}>
+    <div className={classnames(classes.container, className)} style={{ margin, display }}>
       <code className={classes.code}>{children}</code>
     </div>
   );
